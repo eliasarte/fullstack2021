b4d7b8b7abb5e28aad059a5d0ce20006e4089345
Elias <elias.arte@live.com>
2021-05-06 17:02:12 +0300

    osa2 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+const Person = ( {person} ) => {
+  return (
+    <p>{person.name}</p>
+  )
+}
+
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const personObject = {
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map(person => 
+          <Person key={person.name} person={person} />
+        )}
+    </div>
+  )
+
+}
+
+export default App
